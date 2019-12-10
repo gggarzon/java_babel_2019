f69de84e2df7c8360a3e97d1ef0d6242cc08bc69
gggarzon <g.ggarzon@alumnos.upm.es>
2019-12-10 23:42:17 +0100

    Añadido metodos facturacion

--- 03_gestion_pedidos/src/principal/Facturacion.java
+++ 03_gestion_pedidos/src/principal/Facturacion.java
@@ -6,26 +6,38 @@ import java.util.Scanner;
 import model.Pedido;
 
 public class Facturacion {
-
+	static Scanner sc = new Scanner(System.in);
+	static ArrayList <Pedido> pedidos = new ArrayList<>(5);
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList <Pedido> pedidos = new ArrayList<>();
-		String nombre;
-		String unidades;
-		String precio;
-		for(int i = 0; i<pedidos.size();i++ ) {
-			System.out.println("Introduce el nombre del producto:");
-			nombre = sc.nextLine();
-			System.out.println("Introduce unidades:");
-			unidades= sc.nextLine();
-			System.out.println("Introduce el precio");
-			precio = sc.nextLine();
+		System.out.println("Introduce datos del pedido:");
+		datos();
+		facturacion();
+	}
+		public static void datos() {
+			String nombre;
+			String unidades;
+			String precio;
+			for(int i = 0; i<2;i++ ) {
+				System.out.println("Introduce el nombre del producto:");
+				nombre = sc.nextLine();
+				System.out.println("Introduce unidades:");
+				unidades= sc.nextLine();
+				System.out.println("Introduce el precio");
+				precio = sc.nextLine();
+				
+				int unidades2 = Integer.parseInt(unidades);
+				double precio2 = Double.parseDouble(precio);
+				pedidos.add(new Pedido(nombre,unidades2,precio2));
+			}
+	}
+		public static void facturacion() {
+			double suma = 0;
 			
-			pedidos.add(new Pedido(nombre,unidades,precio));
+			for (int i=0; i <pedidos.size();i++) {
+				suma = suma + (pedidos.get(i).getPrecio() * pedidos.get(i).getUnidades());
+			}
+			
+			System.out.println("La facturacion es: "+ suma);
 		}
-	}
-		public void facturacion(Arraylist <Pedido> pedidos) {
-			
-	}
 
 }
